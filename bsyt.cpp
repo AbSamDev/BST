d660dcace103394bfc2f07a45e848a1cf184fc96
Abdul Samad <53661372+abdulsamad-optimisticstr@users.noreply.github.com>
2019-11-09 14:29:52 +0500

    gettingStarted

---
+++ bsyt.cpp
@@ -0,0 +1,19 @@
+// This program is created by Abdul Samad 
+// Getting started with BST (Binary Search Tree)
+
+#include <iostream>
+using namespace std;
+
+class bstNode{
+	public:
+		int data;
+		bstNode *left;
+		bstNode *right;
+	//	bstNode *parent;
+	
+	insert(int data);
+};
+
+bstNode :: inserter(int data){
+	
+}

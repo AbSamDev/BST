08eaba62304cc42d6515df4938787af47feb4320
Abdul Samad <53661372+abdulsamad-optimisticstr@users.noreply.github.com>
2019-11-12 14:59:12 +0500

    Working on BST

--- bsyt.cpp
+++ bsyt.cpp
@@ -4,17 +4,55 @@
 #include <iostream>
 using namespace std;
 
-class bstNode{
+class tNode
+{
+public:
+    int data;
+    tNode * Left;
+    tNode * Right;
+    //tNode * Parent;
+    };
+class BST{
+	tNode *root;
 	public:
-		int data;
-		bstNode *left;
-		bstNode *right;
-	//	bstNode *parent;
-	
-	insert(int data);
-	delete(int data);
-};
+		BST(){
+			root=Null;
+		}
 
-bstNode :: inserter(int data){
-	
+node* insert(int x, node* t)
+    {
+        if(t == NULL)
+        {
+            t = new node;
+            t->data = x;
+            t->left = t->right = NULL;
+        }
+        else if(x < t->data)
+            t->left = insert(x, t->left);
+        else if(x > t->data)
+            t->right = insert(x, t->right);
+        return t;
+    }
+
+int search(int d, tNode *t=root){
+	if(t==Null)
+	return 0;
+	else if(t->data==d)
+	return 1;
+	else{
+		if(t->data<d)
+		search(d,t->right);
+		else
+		search(d,t->left);
+	}
 }
+int minimum(tNode *t=root){
+	if(t==Null)
+	return 0;
+	else if(t->left==NUll)
+	return 1;
+	else{
+		minimum(t->left);
+	}
+}
+};
